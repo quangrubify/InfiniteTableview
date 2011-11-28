2ecfa8870dab1b118a537bef8aec2cef9bd28874
quangrubify <quang@rubify.com>
2011-11-28 12:24:42 +0700

    Initial Commit

---
+++ textTests/textTests.h
@@ -0,0 +1,13 @@
+//
+//  textTests.h
+//  textTests
+//
+//  Created by quang on 28/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface textTests : SenTestCase
+
+@end
---
+++ textTests/textTests.m
@@ -0,0 +1,32 @@
+//
+//  textTests.m
+//  textTests
+//
+//  Created by quang on 28/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "textTests.h"
+
+@implementation textTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in textTests");
+}
+
+@end
